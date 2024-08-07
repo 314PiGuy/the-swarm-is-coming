d333454ec7561bf924919510041012f1794f7f18
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-08-07 10:46:36 +0000

    Hopefully this ravioli code works

--- bluetooth.js
+++ bluetooth.js
@@ -1,94 +1,22 @@
-const fish = '27df26c5-83f4-4964-bae0-d7b7cb0a1f54';
-var device;
+const xrpId = '51ff9301-d04e-4a0d-91c9-975fca9cdf95' //uuid of main xrp service
+
+const button = document.getElementById('KavinBorderPatrol');
+button.onclick = connect;
+
+var device
 var server;
 var service;
-var dataLobber;
-var bleUpdateInProgress = false;
-var isConnectedBLE = false;
-var button;
+var XRPcommands = [];
 
-function createBleAgent() {
-    button = document.getElementById('KavinBorderPatrol')
-
-
-    button.onclick = changeBleState;
-    button.ontouchend = changeBleState;
+async function connect(){
+    device = await navigator.bluetooth.requestDevice({ filters: [{ services: [xrpId] }] });
+    server = await device.gatt.connect();
+    service = await server.getPrimaryService(xrpId);
+    XRPcommands.push(await service.getCharacteristic('ed59696a-b609-4cea-a09a-5885cce3c5ca')); //command characteristic
 }
 
-function displayBleStatus(status) {
-    button.innerHTML = status;
-    switch (status) {
-        case 'Connecting':
-            button.style.backgroundColor = 'grey';
-            break;
-        case 'Connected':
-            button.style.backgroundColor = '#4dae50';
-            break;
-        case 'Disconnecting':
-            button.style.backgroundColor = 'grey';
-            break;
-        case 'Not Connected':
-            button.style.backgroundColor = 'grey';
-            break;
-        default:
-            button.style.backgroundColor = '#eb5b5b';
-    }
-}
-
-
-async function changeBleState() {
-    if (bleUpdateInProgress) return
-    bleUpdateInProgress = true;
-    if (!isConnectedBLE) connectBLE();
-    else disconnectBLE();
-    bleUpdateInProgress = false;
-}
-
-async function connectBLE() {
-    displayBleStatus('Connecting');
-
-    try {
-        device = await navigator.bluetooth.requestDevice({ filters: [{ services: [fish] }] });
-        server = await device.gatt.connect();
-        service = await server.getPrimaryService(fish);
-        dataLobber = service.getCharacteristic('27df26c6-83f4-4964-bae0-d7b7cb0a1f54')
-        await device.addEventListener('gattserverdisconnected', robotDisconnect);
-
-        displayBleStatus('Connected');
-        isConnectedBLE = true;
-
-    } catch (error) {
-        displayBleStatus("Error");
-        console.error('Error:', error);
-    }
-}
-
-async function disconnectBLE() {
-    displayBleStatus('Disconnecting');
-    try {
-        await device.gatt.disconnect();
-
-        displayBleStatus('Not Connected');
-        isConnectedBLE = false;
-
-    } catch (error) {
-        displayBleStatus("Error");
-        console.error('Error:', error);
-    }
-}
-
-function robotDisconnect(event) {
-    displayBleStatus('Not Connected');
-    isConnectedBLE = false;
-}
-
-async function sendPacketBLE(byteArray) {
-    if (!isConnectedBLE) return;
-    if (bleUpdateInProgress) return;
-
-    try {
-        await dataLobber.writeValueWithoutResponse(byteArray);
-    } catch (error) {
-        console.error('Error:', error);
+async function send(data){
+    for (var c in XRPcommands){
+        c.writeValueWithoutResponse();
     }
 }
